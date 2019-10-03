77e5dd1956a8815aabf3685fade9d5adf0d07d99
Sean Dwyer <ewetoo@gmail.com>
2019-10-03 16:56:13 +1000

    initial changes to start updating from release 6.0.12

--- fetch_vbox_headers.sh
+++ fetch_vbox_headers.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )" 
-svn co http://www.virtualbox.org/svn/vbox/trunk/include@42039 ${SCRIPT_DIR}/include/
+# grab per revision, eg @81014
+# check with https://www.virtualbox.org/browser/vbox/trunk be careful about
+# bleeding edge versions!
+svn co http://www.virtualbox.org/svn/vbox/trunk/include@81014 ${SCRIPT_DIR}/include/
